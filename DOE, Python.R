8a1855fc9eeb290c0b3a9fb9094c3f8fc9f6d00f
Øyvind Klåpbakken <oyvind.klaapbakken@gmail.com>
2017-04-22 15:39:59 +0200

    R Script for Analysis

---
+++ DOE, Python.R
@@ -0,0 +1,30 @@
+rm(list=ls())
+
+library(FrF2)
+library(nortest)
+newrun = FALSE
+
+if (newrun){
+  factor_names <- c("A", "B", "C", "D")
+  plan <- FrF2(nruns=16,nfactors=4,factor.names=factor_names)
+  plan
+  write.csv(plan, file="plan.csv")
+  save(plan,file="plan.RData")
+} else {
+  load(plan)
+}
+
+data <- read.csv(file="mock_data.csv")
+attach(data)
+plan <- add.response(plan, y)
+lm <- lm(y~(.)^3, data=plan)
+summary(lm)
+
+
+effects <- 2*lm$coeff
+MEPlot(lm)
+IAPlot(lm)
+qqnorm(rstudent(lm),pch=20)
+qqline(rstudent(lm))
+plot(lm$fitted,rstudent(lm),pch=20)
+cubePlot(lm,"A","B","C",round=1,size=0.33,main="") 
